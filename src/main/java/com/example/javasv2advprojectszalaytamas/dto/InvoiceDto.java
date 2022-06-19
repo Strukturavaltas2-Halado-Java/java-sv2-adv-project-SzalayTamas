249645471130e5bf69db8c5d1f8d54e6cdf8db1e
Szalay Tamás <szalaytamas90@gmail.com>
2022-06-19 14:33:07 +0200

    dto fix

--- src/main/java/com/example/javasv2advprojectszalaytamas/dto/InvoiceDto.java
+++ src/main/java/com/example/javasv2advprojectszalaytamas/dto/InvoiceDto.java
@@ -1,17 +1,14 @@
 package com.example.javasv2advprojectszalaytamas.dto;
 
-import com.example.javasv2advprojectszalaytamas.entity.Customer;
-import com.example.javasv2advprojectszalaytamas.entity.Meter;
 import com.example.javasv2advprojectszalaytamas.entity.Status;
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -24,21 +21,16 @@ public class InvoiceDto {
     @Schema(description = "The invoice belongs to this customer ")
     @JsonIgnoreProperties("invoices")
     private Long customer;
-    @Schema(description = "The customer is getting billed for that amount of electricity", example = "13")
-    private double usedElectricity;
     @Schema(description = "Shows the invoice status can be pending or paid")
     private Status status;
     @Schema(description = "The customer have to pay that amount of money for the used electricity", example = "1")
     private double debt;
-    @Schema(description = "The date of the creation this invoice" , example = "2022-06-10T16:15:39.9627303")
+    @Schema(description = "The date of the creation this invoice", example = "2022-06-10T16:15:39.9627303")
     private LocalDateTime dateOfInvoiceCreation;
     @Schema(description = "The price customer have to pay for electricity per kilowatt ")
     private double pricePerKiloWatt;
-
-
-
-
-
+    @Schema(description = "The customer is getting billed for that amount of electricity", example = "13")
+    private double billed_electricity;
 
 
 }
